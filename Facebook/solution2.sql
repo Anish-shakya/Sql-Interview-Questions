246ccd8ba46fc2627ce6219dd7a86b36eca4d586
Anish Shakya <64535767+Anish-shakya@users.noreply.github.com>
2023-03-13 20:07:41 +0545

    Create solution2.sql

---
+++ Facebook/solution2.sql
@@ -0,0 +1,9 @@
+SELECT
+  app_id,
+  ROUND(100.0 *
+    SUM(CASE WHEN event_type = 'click' THEN 1 ELSE 0 END) /
+    SUM(CASE WHEN event_type = 'impression' THEN 1 ELSE 0 END), 2)  AS ctr_rate
+FROM events
+WHERE timestamp >= '2022-01-01' 
+  AND timestamp < '2023-01-01'
+GROUP BY app_id;
